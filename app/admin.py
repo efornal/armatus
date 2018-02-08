0f9dd2ca8079728eb8a4dfb7a72c851c1e2627ba
Esteban Fornal <efornal@rectorado.unl.edu.ar>
2018-02-08 12:36:26 -0300

    Create model Service

--- app/admin.py
+++ app/admin.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from app.models import Check
+from app.models import Service
 
-# Register your models here.
-    
 class CheckAdmin(admin.ModelAdmin):
     ordering = ('-created_at','-updated_at')
     list_display = ('created_at','start_time','end_time')
+
+class ServiceAdmin(admin.ModelAdmin):
+    ordering = ('-created_at','-updated_at')
+    list_display = ('created_at','hours','oil')
     
 admin.site.register(Check,CheckAdmin)
+admin.site.register(Service,ServiceAdmin)
--- app/models.py
+++ app/models.py
@@ -68,3 +68,62 @@ class Check(models.Model):
         m = (ts%3600)//60
         s = (ts%3600)%60
         return "{}:{}".format(int(h),int(m))
+
+
+class Service(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        null=False)
+    user = models.ForeignKey(
+        User,
+        null=False,
+        default=None,
+        verbose_name=_('user'))
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('created_at'))
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_('updated_at'))
+    hours = models.IntegerField(
+        null=False,
+        verbose_name=_('hours'))
+    oil_type = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True,
+        verbose_name=_('oil_type'))
+    oil = models.BooleanField(
+        default=False,
+        verbose_name=_('oil'))
+    oil_filter = models.BooleanField(
+        default=False,
+        verbose_name=_('oil_filter'))
+    fuel_filter = models.BooleanField(
+        default=False,
+        verbose_name=_('fuel_filter'))
+    air_filter = models.BooleanField(
+        default=False,
+        verbose_name=_('air_filter'))
+    refrigerant = models.BooleanField(
+        default=False,
+        verbose_name=_('refrigerant'))
+    battery = models.BooleanField(
+        default=False,
+        verbose_name=_('battery '))
+    observations = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name=_('observations'))
+
+    class Meta:
+        db_table = 'services'
+        verbose_name = _('Service')
+        verbose_name_plural = _('Services')
+
+    def __unicode__(self):
+        return unicode(format(self.created_at, "%d-%m-%Y, %H:%S"))
+
+    def __str__(self):
+        return format(self.created_at, "%d-%m-%Y, %H:%S")
+    
